b2824ee58584b8c4d22130ba11581772faf7a1b1
Ben Goodrow <bpgoodrow@gmail.com>
2022-06-30 11:37:13 -0700

    Before lunch commit
    Co-authored-by: Frank Timmons <franktimmonspdx@gmail.com>

--- src/bruzu-request.js
+++ src/bruzu-request.js
@@ -1,7 +1,7 @@
 export class BruzuRequest {
-  static async getBruzu(text){
+  static async getBruzu(text, textColor, bgColor, fontFamily, textAlign, fontSize){
     try {
-      const response = await fetch(`https://img.bruzu.com/?a.text=${text}`);
+      const response = await fetch(`https://img.bruzu.com/?a.text=${text}&a.color=${textColor}&backgroundColor=${bgColor}&a.fontFamily=${fontFamily}&a.textAlign=${textAlign}&a.fontSize=${fontSize}`);
       if (!response.ok) {
         throw Error(response.statusText);
       }
--- src/index.js
+++ src/index.js
@@ -10,15 +10,20 @@ function getElementsBruzu(response){
   $('#imageOutput').html(`<img src="${image.url}">`)
 }
 
-async function makeBruzuApiCall(text){
-  const response = await BruzuRequest.getBruzu(text);
+async function makeBruzuApiCall(text, textColor, bgColor, fontFamily, textAlign, fontSize){
+  const response = await BruzuRequest.getBruzu(text, textColor, bgColor, fontFamily, textAlign, fontSize);
   getElementsBruzu(response);
 }
 
 $(document).ready(function(){
   $('form#imageMaker').submit(function(event){
-    let text = $('#text').val()
+    let text = $('#text').val();
+    let textColor = $('#textColor').val();
+    let bgColor = $('#backgroundColor').val();
+    let fontFamily = $('#fontFamily').val();
+    let textAlign = $('#textAlign').val();
+    let fontSize = $('#fontSize').val();
     event.preventDefault();
-    makeBruzuApiCall(text);
+    makeBruzuApiCall(text, textColor, bgColor, fontFamily, textAlign, fontSize);
   });
 });
